cef46e7f8f4a0d7deb2183d19b8f4435eaf5d2e0
Вячеслав <95027060+vyacheslav-79109734555@users.noreply.github.com>
2022-04-07 19:28:59 +0300

    Анализ слова

---
+++ _Домашнее задание/task_09_word_analysis_2/9_Анализ слова_2.py
@@ -0,0 +1,43 @@
+# топот
+# я иду с мечем судия
+# Делу - тело, лету - лед
+# А за работу дадут? - Оба раза!
+
+def palindrome(s):
+    s_palin = ''
+    for i in range(len(s) - 1, -1, -1):
+        s_palin += s[i]
+    return s == s_palin
+
+
+# ********************************
+txt_str = input('Введите строку: ')
+txt_set = palindrome(txt_str)
+print('txt_set', txt_set)
+
+
+# TODO ******************************** хорошее решение
+def palindrome(s):
+    wrong = [' ', '.', ',', '!', '?', ':', ';', '-']
+    s = s.lower()
+    for symbol in wrong:
+        s = s.replace(symbol, '')
+    return s == s[:: -1]
+
+
+print(palindrome(txt_str))
+
+
+# ********************************
+def palindrome(s):
+    wrong = [' ', '.', ',', '!', '?', ':', ';', '-']
+    s = s.lower()
+    for symbol in wrong:
+        s = s.replace(symbol, '')
+    for i in range(len(s) // 2):
+        if s[i] != s[-i - 1]:
+            return False
+    return True
+
+
+print(palindrome(txt_str))
